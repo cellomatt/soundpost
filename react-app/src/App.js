ff9818eaa4db83cb9626c31532925dd2cf380e5c
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-03 00:11:43 -0600

    deleted unused routes and components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -39,9 +39,6 @@ function App() {
         <Route path="/sign-up" exact={true}>
           <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
         </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList/>
-        </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
           <User />
         </ProtectedRoute>
