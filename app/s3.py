1b061862e1ded36a952486bf4201e31c712aa8a0
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-02 19:16:38 -0600

    added s3 helper routes

---
+++ app/s3.py
@@ -0,0 +1,36 @@
+import boto3, botocore
+from .config import Config
+
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=Config.S3_KEY,
+   aws_secret_access_key=Config.S3_SECRET
+)
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=Config.S3_KEY,
+   aws_secret_access_key=Config.S3_SECRET
+)
+
+def upload_file_to_s3(file, bucket_name, acl="public-read"):
+
+    try:
+
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        print("Something Happened: ", e)
+        return e
+
+
+    return f"{Config.S3_LOCATION}{file.filename}"
