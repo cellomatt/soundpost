5ff2ab728b4452257372e172758bd3e680d377cb
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-04 16:49:28 -0600

    working on conditional scroll styles

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css'
 import {ReactComponent as Logo} from './logo.svg'
 
+
 const NavBar = ({ authenticated, setAuthenticated }) => {
+  const [scrolled, setScrolled] = useState(false)
+  window.onscroll = function() {scroller()};
+
+  function scroller() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    // let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    if (winScroll > 160) {
+      setScrolled(true);
+    } else { setScrolled(false); }
+    console.log(winScroll);
+    // document.getElementById("myBar").style.width = scrolled + "%";
+  }
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${scrolled ? "navbar-scrolled" : ""}`}>
       <Link className="logo" to="/">
         <Logo  />
       </Link>
