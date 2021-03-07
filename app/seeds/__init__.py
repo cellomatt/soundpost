d8b6a23ce5c220fc9b3e6c56ddfcaad3da406651
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-07 15:03:09 -0600

    successfully seeding a month's worth of assignments (for Wednesday a month before 'today'), need to update messages

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -4,13 +4,13 @@ from .teachers import seed_teachers, undo_teachers
 from .states import seed_states, undo_states
 from .practice_logs import seed_practice_logs, undo_practice_logs
 from .timeslots import seed_timeslots, undo_timeslots
+from .assignments import seed_assignments, undo_assignments
 
-# Creates a seed group to hold our commands
-# So we can type `flask seed --help`
+# Create a seed group to hold commands
 seed_commands = AppGroup('seed')
 
 
-# Creates the `flask seed all` command
+# Create the `flask seed all` command
 @seed_commands.command('all')
 def seed():
     seed_states()
@@ -18,13 +18,14 @@ def seed():
     seed_students()
     seed_practice_logs()
     seed_timeslots()
-
+    seed_assignments()
     # Add other seed functions here
 
 
-# Creates the `flask seed undo` command
+# Create the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_assignments()
     undo_timeslots()
     undo_practice_logs()
     undo_students()
--- app/seeds/assignments.py
+++ app/seeds/assignments.py
@@ -1,9 +1,47 @@
-from app.models import db, Assignment
+from app.models import db, Assignment, Student, Teacher
+from datetime import *
+from dateutil.relativedelta import *
 
 
 def seed_assignments():
-    pass
+    teacher = Teacher.query.filter(Teacher.first_name == "Matt").first()
+    student = Student.query.filter(Student.first_name == "Demo").first()
+    today = date.today()
+    first_lesson = today + relativedelta(months=-1, weekday=WE)
+    first_assignment_time = datetime.combine(first_lesson, time(hour=16))
+
+    assignment1 = Assignment(
+        student_id=student.id, teacher_id=teacher.id,
+        message="Great first lesson, Demo! Here\'s what I\'d like you work on this week: 10 minutes a day on 1 octave G major scale and 2 octave C major scale with special concentration on keeping your bow straight and producing a big sound. Please also prepare Allegro from Suzuki Book 1, we will look at that next week!",
+        created_at=first_assignment_time
+    )
+
+    assignment2 = Assignment(
+        student_id=student.id,
+        teacher_id=teacher.id,
+        message="Great first lesson, Demo! Here\'s what I\'d like you work on this week: 10 minutes a day on 1 octave G major scale and 2 octave C major scale with special concentration on keeping your bow straight and producing a big sound. Please also prepare Allegro from Suzuki Book 1, we will look at that next week!",
+        created_at=first_assignment_time + timedelta(days=7)
+    )
+    assignment3 = Assignment(
+        student_id=student.id,
+        teacher_id=teacher.id,
+        message="Great first lesson, Demo! Here\'s what I\'d like you work on this week: 10 minutes a day on 1 octave G major scale and 2 octave C major scale with special concentration on keeping your bow straight and producing a big sound. Please also prepare Allegro from Suzuki Book 1, we will look at that next week!",
+        created_at=first_assignment_time + timedelta(days=14)
+    )
+    assignment4 = Assignment(
+        student_id=student.id,
+        teacher_id=teacher.id,
+        message="Great first lesson, Demo! Here\'s what I\'d like you work on this week: 10 minutes a day on 1 octave G major scale and 2 octave C major scale with special concentration on keeping your bow straight and producing a big sound. Please also prepare Allegro from Suzuki Book 1, we will look at that next week!",
+        created_at=first_assignment_time + timedelta(days=21)
+    )
+
+    db.session.add(assignment1)
+    db.session.add(assignment2)
+    db.session.add(assignment3)
+    db.session.add(assignment4)
+    db.session.commit()
 
 
 def undo_assignments():
-    pass
+    db.session.execute('TRUNCATE timeslots RESTART IDENTITY CASCADE;')
+    db.session.commit()
