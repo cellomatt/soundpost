a9ae06665adb861dd86061bc2444a31fa8229309
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-13 20:43:31 -0600

    formatted stats on stats page and added area for practice log

--- react-app/src/components/Stats/index.js
+++ react-app/src/components/Stats/index.js
@@ -6,6 +6,7 @@ import './Stats.css';
 import * as statsActions from '../../store/stats'
 
 export default function Stats() {
+  document.title = "Soundpost — Your Stats"
   const dispatch = useDispatch();
   const user = useSelector(state => state.session.user)
   const stats = useSelector(state => state.stats)
@@ -16,66 +17,76 @@ export default function Stats() {
 
   return (
     <div className="main">
-      <h1 className="title__main">Stats</h1>
+      <h1 className="title__main">Your Stats</h1>
       <div className="stats-main">
-        <div className="user-info__stats stats__container">
-          <h3 className="user-info__stats--label">Days Practiced<br></br>This Week</h3>
-          <div className="graph">
-            <CircularProgressbar
-              value={stats.thisweek.percentage}
-              text={`${stats.thisweek.count}/7`}
-              styles={buildStyles(
-                {
-                  pathColor: "#0061ff",
-                  trailColor: "rgba(190, 190, 190, 0.746)",
-                  textColor: "#0051d4",
-                  fontFamily: "P22BayerW00-Universal"
-                }
-              )}
-              />
+        <div className="stats-main__stats">
+          <div className="user-info__stats stats__container">
+            <h3 className="user-info__stats--label">Days Practiced<br></br>This Week</h3>
+            <div className="graph">
+              <CircularProgressbar
+                value={stats.thisweek.percentage}
+                text={`${stats.thisweek.count}/7`}
+                styles={buildStyles(
+                  {
+                    pathColor: "#0061ff",
+                    trailColor: "rgba(190, 190, 190, 0.746)",
+                    textColor: "#0051d4",
+                    fontFamily: "P22BayerW00-Universal"
+                  }
+                  )}
+                  />
+            </div>
+          </div>
+          <div className="user-info__stats stats__container">
+            <h3 className="user-info__stats--label">Days Practiced<br></br>This Month</h3>
+            <div className="graph">
+              <CircularProgressbar
+                value={stats.thismonth.percentage}
+                text={`${stats.thismonth.percentage}%`}
+                styles={buildStyles(
+                  {
+                    pathColor: "#0061ff",
+                    trailColor: "rgba(190, 190, 190, 0.746)",
+                    textColor: "#0051d4",
+                    fontFamily: "P22BayerW00-Universal"
+                  }
+                  )}
+                  />
+            </div>
+          </div>
+          <div className="user-info__stats stats__container">
+            <h3 className="user-info__stats--label">Days Practiced Since You Joined Soundpost</h3>
+            <div className="graph">
+              <CircularProgressbar
+                value={stats.all.percentage}
+                text={`${stats.all.percentage}%`}
+                styles={buildStyles(
+                  {
+                    pathColor: "#0061ff",
+                    trailColor: "rgba(190, 190, 190, 0.746)",
+                    textColor: "#0051d4",
+                    fontFamily: "P22BayerW00-Universal"
+                  }
+                  )}
+                  />
+            </div>
+          </div>
+          <div className="user-info__stats stats__container">
+            <h3 className="user-info__stats--label">Days on Soundpost</h3>
+            <h1 className="user-info__stats--number">{stats.days}</h1>
+          </div>
+          <div className="user-info__stats stats__container">
+            <h3 className="user-info__stats--label">Lessons Completed</h3>
+            <h1 className="user-info__stats--number">{stats.lessons}</h1>
           </div>
         </div>
-        <div className="user-info__stats stats__container">
-          <h3 className="user-info__stats--label">Days Practiced<br></br>This Month</h3>
-          <div className="graph">
-            <CircularProgressbar
-              value={stats.thismonth.percentage}
-              text={`${stats.thismonth.percentage}%`}
-              styles={buildStyles(
-                {
-                  pathColor: "#0061ff",
-                  trailColor: "rgba(190, 190, 190, 0.746)",
-                  textColor: "#0051d4",
-                  fontFamily: "P22BayerW00-Universal"
-                }
-              )}
-              />
+        <div className="stats-main__logs">
+          <div className="logs__container">
+            <h1 className="title logs__title">Practice Log</h1>
+            <div className="logs__container--inner">
+
+            </div>
           </div>
-        </div>
-        <div className="user-info__stats stats__container">
-          <h3 className="user-info__stats--label">Days Practiced Since You Joined Soundpost</h3>
-          <div className="graph">
-            <CircularProgressbar
-              value={stats.all.percentage}
-              text={`${stats.all.percentage}%`}
-              styles={buildStyles(
-                {
-                  pathColor: "#0061ff",
-                  trailColor: "rgba(190, 190, 190, 0.746)",
-                  textColor: "#0051d4",
-                  fontFamily: "P22BayerW00-Universal"
-                }
-              )}
-              />
-          </div>
-        </div>
-        <div className="user-info__stats stats__container">
-          <h3 className="user-info__stats--label">Days on Soundpost</h3>
-          <h1 className="user-info__stats--number">{stats.days}</h1>
-        </div>
-        <div className="user-info__stats stats__container">
-          <h3 className="user-info__stats--label">Lessons Completed</h3>
-          <h1 className="user-info__stats--number">{stats.lessons}</h1>
         </div>
       </div>
     </div>
