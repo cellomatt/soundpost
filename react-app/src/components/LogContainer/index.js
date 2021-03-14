17f97e998949a68e4ff993be23a40c214b931284
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-14 13:05:39 -0500

    added modal to practice log, needs backend functionality

--- react-app/src/components/LogContainer/index.js
+++ react-app/src/components/LogContainer/index.js
@@ -1,12 +1,15 @@
-// import {useState} from 'react'
+import {useState} from 'react'
+import PracticeModal from '../PracticeModal'
 import './LogContainer.css'
 
-export default function LogContainer({date, practiced}) {
+export default function LogContainer({date, practiced, setChange, student}) {
+  const [practice, setPractice] = useState(practiced)
 
   return (
     <div className="log__container">
     <p className="log__container--date">{date}</p>
-    <p className="log__container--practiced">Practiced: {practiced ? <i className="far fa-check-square green"></i> : <i className="fas fa-times-circle red"></i>}</p>
+    <p className="log__container--practiced">Practiced: {practice ? <i className="far fa-check-square green"></i> : <i className="fas fa-times-circle red"></i>}</p>
+    <PracticeModal practice={practice} setPractice={setPractice} date={date} setChange={setChange} student={student}/>
     </div>
   )
 }
