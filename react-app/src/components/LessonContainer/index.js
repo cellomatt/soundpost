5d0e24780663d26304360e37b3251d472cdcfe8f
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-22 17:42:52 -0500

    added address to lesson slots

--- react-app/src/components/LessonContainer/index.js
+++ react-app/src/components/LessonContainer/index.js
@@ -21,6 +21,8 @@ export default function LessonContainer({lesson, setChange, duration}) {
         <div className="lesson__container--date">{lesson.start_time.toLocaleDateString('en-US', options)}</div>
         <div className="lesson__container--time">{lesson.start_time.toLocaleTimeString('en-US', timeOptions)} - {lesson.end_time.toLocaleTimeString('en-US', timeOptions)}</div>
         <div className="lesson__container--teacher">Teacher: {lesson.teacher.first_name} {lesson.teacher.last_name}</div>
+        <div className="lesson__container--teacher">Location: {lesson.teacher.street_address}</div>
+        <div className="lesson__container--teacher">{lesson.teacher.city}, {lesson.teacher.state} {lesson.teacher.zip}</div>
       </div>
       <div className="lesson__container--modal">
         <LessonModal lesson={lesson} scheduled={scheduled} setScheduled={setScheduled} setChange={setChange} duration={duration} student={student}/>
